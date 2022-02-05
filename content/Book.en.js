b70a342d087e63d73ba00b8b73530b630ab15cca
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2022-02-05 03:20:47 +0200

    Add config files

---
+++ content/Book.en.js
@@ -0,0 +1,6 @@
+module.exports = {
+  title: 'Metaprogramming',
+  subtitle: 'Multi-paradigm approach in the Software Engineering',
+  copyright: '© Timur Shemsedinov, Metarhia community',
+  location: 'Kiev, 2015 — 2022',
+};
