b3ed9ee19b6541274b917b76ac3c89d69354f49c
Stanislav Tursky <stastursky@gmail>
2022-02-07 01:31:41 +0200

    Add Czech texts to Front page

--- content/Book.cz.js
+++ content/Book.cz.js
@@ -1,6 +1,6 @@
 module.exports = {
-  title: '',
-  subtitle: '',
-  copyright: '',
-  location: '',
+  title: 'Metaprogramování',
+  subtitle: 'Multiparadigmatický přístup v softwarovém inženýrství',
+  copyright: '© Timur Shemsedinov, Společenství Metarhia',
+  location: 'Kyjev, 2015 — 2022',
 };
