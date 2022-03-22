01a3bb53de7dba6392c6d7b0bf5de24ef7ec9e7e
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2022-03-22 03:11:28 +0200

    Build multiple languages

--- build.js
+++ build.js
@@ -6,92 +6,96 @@ const fs = require('fs');
 const PdfPrinter = require('pdfmake');
 const config = require('./content/Book.js');
 const languages = Object.keys(config.languages);
-
 const printer = new PdfPrinter(config.fonts);
-const content = [];
-const lang = 'en';
 
-const front = config.languages[lang];
+const generate = (lang) => {
+  const content = [];
+  const front = config.languages[lang];
 
-content.push({ text: front.title, ...config.title });
-content.push({ text: front.subtitle, ...config.subtitle });
-content.push({ text: front.copyright, ...config.copyright });
-content.push({ text: front.location, ...config.location });
+  content.push({ text: front.title, ...config.title });
+  content.push({ text: front.subtitle, ...config.subtitle });
+  content.push({ text: front.copyright, ...config.copyright });
+  content.push({ text: front.location, ...config.location });
 
-const caption = (s) => {
-  content.push({ text: s, ...config.caption });
-};
+  const caption = (s) => {
+    content.push({ text: s, ...config.caption });
+  };
 
-const index = (s) => {
-  content.push({ text: s, ...config.index });
-};
+  const index = (s) => {
+    content.push({ text: s, ...config.index });
+  };
 
-const para = (s) => {
-  content.push({ text: s, ...config.para });
-};
+  const para = (s) => {
+    content.push({ text: s, ...config.para });
+  };
 
-const code = (src) => {
-  content.push({
-    table: {
-      widths: ['*'],
-      body: [
-        [
-          {
-            margin: [30, 5, 30, 5],
-            text: src,
-          },
+  const code = (src) => {
+    content.push({
+      table: {
+        widths: ['*'],
+        body: [
+          [
+            {
+              margin: [30, 5, 30, 5],
+              text: src,
+            },
+          ],
         ],
-      ],
-    },
-    ...config.code,
-  });
-};
+      },
+      ...config.code,
+    });
+  };
 
-const section = (name) => {
-  content.push({ text: '', pageBreak: 'before' });
-  const src = fs.readFileSync(`content/${name}.${lang}.md`, 'utf8');
-  const rows = src.split('\n');
-  let lines = [];
-  for (const row of rows) {
-    if (row.startsWith('#')) {
-      const text = row.replace('#', '');
-      caption(text);
-    } else if (row.trim().substring(0, 3).includes('.')) {
-      index(row);
-    } else if (row === '') {
-      para(lines.join(' '));
-      lines = [];
-    } else {
-      lines.push(row);
+  const section = (name) => {
+    content.push({ text: '', pageBreak: 'before' });
+    const src = fs.readFileSync(`content/${name}.${lang}.md`, 'utf8');
+    const rows = src.split('\n');
+    let lines = [];
+    for (const row of rows) {
+      if (row.startsWith('#')) {
+        const text = row.replace(/#/g, '');
+        caption(text);
+      } else if (row.trim().substring(0, 3).includes('.')) {
+        index(row);
+      } else if (row === '') {
+        para(lines.join(' '));
+        lines = [];
+      } else {
+        lines.push(row);
+      }
     }
+  };
+
+  for (const name of config.sections) {
+    section(name);
   }
+
+  /*
+  para('Consider following:');
+  code(`const id = (x) => x;
+
+  // Usage
+
+  const res = id(5);
+  console.log({ res });`);
+  */
+
+  const book = printer.createPdfKitDocument({
+    content,
+    pageSize: 'A5',
+    pageMargins: [0, 20, 0, 20],
+    footer: (currentPage) => ({
+      text: currentPage.toString(),
+      alignment: 'center',
+    }),
+  });
+
+  book.pipe(fs.createWriteStream(`release/Metaprogramming-${lang}.pdf`));
+  book.end();
 };
 
-for (const name of config.sections) {
-  section(name);
+for (const lang of languages) {
+  generate(lang);
 }
-
-/*
-para('Consider following:');
-code(`const id = (x) => x;
-
-// Usage
-
-const res = id(5);
-console.log({ res });`);
-*/
-
-const book = printer.createPdfKitDocument({
-  content,
-  pageSize: 'A5',
-  pageMargins: [0, 20, 0, 20],
-  footer: (currentPage) => ({
-    text: currentPage.toString(),
-    alignment: 'center',
-  }),
-});
-
-book.pipe(fs.createWriteStream('book.pdf'));
-book.end();
 
 console.log('Time spent:', new Date() - now);
