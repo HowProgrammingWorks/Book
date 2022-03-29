662d409bfe6e3fc56916e054d350780c216338b4
Volodymyr Polyatichko <testrorsh123@gmail.com>
2022-03-29 18:52:37 +0300

    implement Styles

--- content/codeHighlight.js
+++ content/codeHighlight.js
@@ -4,8 +4,46 @@ const hljs = require('highlight.js');
 const css = require('css');
 const fs = require('fs');
 
+const TEMP_PATH_STYLE = './node_modules/highlight.js/styles/atom-one-light.css';
+
 class Styles {
-  constructor() {}
+  constructor() {
+    const content = fs.readFileSync(TEMP_PATH_STYLE).toString();
+    const parsed = css.parse(content, {});
+    const stylesheet = parsed.stylesheet.rules;
+    this.css = {};
+    for (const rule of stylesheet) {
+      const { type, selectors, declarations } = rule;
+
+      if (type === 'rule') {
+        const styles = {};
+        for (const declaration of declarations) {
+          const { property, value } = declaration;
+          console.dir({ property, value });
+          if (
+            ['font-weight', 'font-style'].some((e) => e === property) &&
+            ['bold', 'italic'].some((e) => e === value)
+          ) {
+            styles[value] = true;
+          } else if (
+            property === 'text-decoration' &&
+            ['underline', 'lineThrough', 'overline'].some((e) => e === value)
+          ) {
+            styles.decoration = value;
+          } else if (
+            ['color', 'background'].some((e) => e === property) &&
+            ['url', 'rgb'].some((e) => !value.includes(e))
+          ) {
+            styles[property] = value;
+          }
+        }
+
+        for (const selector of selectors) {
+          selector.split(' ').forEach((s) => { this.css[s] = styles; });
+        }
+      }
+    }
+  }
 }
 
 class PDFRenderer extends Styles {
@@ -23,7 +61,9 @@ class TokenTreeEmitter extends TokenTree {
     super();
   }
 
-  toHTML() { return this.toPDF(); }
+  toHTML() {
+    return this.toPDF();
+  }
 
   toPDF() {
     const renderer = new PDFRenderer(this, this.options);
